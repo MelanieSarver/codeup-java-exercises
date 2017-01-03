76b41f6a0dda73b504d1f3bd47d4c0ae743cc9e0
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-03 12:15:24 -0600

    Created new project for codeup java exercises. Created new Hello World Java file.

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by mel on 1/3/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Howdy, world!");
+        System.out.println("All Java code lives inside of classes.");
+        System.out.println("Our program runs the 'main' method when executed.");
+        System.out.println("goodbye, World!");
+    }
+}
