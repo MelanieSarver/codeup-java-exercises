a544daf27591d1ca0f96a8b1d690f16d620b48de
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-03 15:03:58 -0600

    Exercise 5.1 completed

---
+++ src/TypesDemo.java
@@ -0,0 +1,38 @@
+/**
+ * Created by mel on 1/3/17.
+ */
+public class TypesDemo {
+//    ToDo: create variables of every primitive type and print them to the console
+//    ToDo: try to assign a character to a variable that has been declared as an int
+//    ToDo: try to define a variable with the name class
+//    ToDo: initialize a variable, but do not assign it a value, then try and print it to the console
+
+    public static void main(String[] args) {
+        byte thisByte = Byte.MIN_VALUE;
+        short thisShort = 2012;
+        int thisInt = 8;
+        long thisLong = 2008975689747320L;
+        float thisFloat = (float) 3.14;
+        double thisDouble = 0.8758936;
+        char thisChar = 'n';
+        boolean thisBoolean = false;
+
+        System.out.println(thisByte);
+        System.out.println(thisShort);
+        System.out.println(thisInt);
+        System.out.println(thisLong);
+        System.out.println(thisFloat);
+        System.out.println(thisDouble);
+        System.out.println(thisChar);
+        System.out.println(thisBoolean);
+
+//        thisInt = 'm';
+//        System.out.println(thisInt);
+
+//        short class = 87;
+//        System.out.println(class);
+
+//        int anotherInt;
+//        System.out.println(anotherInt);
+    }
+}
