8eaa23e067d0a93fcb51da45752e6f637711f166
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-04 13:58:10 -0600

    Exercise 5.3  all exercises complete

--- src/LoopBasics.java
+++ src/LoopBasics.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by mel on 1/4/17.
  */
@@ -11,6 +13,12 @@ public class LoopBasics {
         }
 
 // Exercise ii Do While Loops
+        int even = 0;
+        do {
+            even = even + 2;
+            System.out.println(even);
+        } while (even <= 100);
+
         int fives = 100;
         do {
             System.out.println(fives);
@@ -40,5 +48,27 @@ public class LoopBasics {
             System.out.println(s);
         }
 
+// Exercise 3 Table of Powers
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int userNumber = sc.nextInt();
+        for (int e = 1; e <= userNumber; e++ ) {
+                System.out.println(e + " squared = " + Math.pow(e, 2) + " and " + e + " cubed = " + Math.pow(e, 3));
+        }
+
+// Exercise 4 Convert grades
+        System.out.println("What score did you get on the exam?");
+        int userGrade = sc.nextInt();
+        if (userGrade <= 100 && userGrade >= 88) {
+            System.out.println("Yay! You got an A!!");
+        } else if (userGrade <= 87 && userGrade >= 80){
+            System.out.println("Good job, you got a B. Study harder next time");
+        } else if (userGrade <= 79 && userGrade >=67) {
+            System.out.println("You got a C ? You're grounded.");
+        } else if (userGrade <= 66 && userGrade >= 60) {
+            System.out.println("You got a D ? Just wait 'til your Father gets home.");
+        } else {
+            System.out.println("You failed. Enjoy military school.");
+        }
     }
 }
