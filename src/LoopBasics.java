e14e30685b59f81becc9d940dc92051102463e8d
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-04 12:11:14 -0600

    Exercise 5.3  i. While complete  ii Do While complete iii For Loops complete

--- src/LoopBasics.java
+++ src/LoopBasics.java
@@ -3,12 +3,14 @@
  */
 public class LoopBasics {
     public static void main(String[] args) {
+// Exercise i While Loop
         int i = 5;
         while (i <= 15) {
             System.out.println(i);
             i ++;
         }
 
+// Exercise ii Do While Loops
         int fives = 100;
         do {
             System.out.println(fives);
@@ -21,8 +23,22 @@ public class LoopBasics {
             squared = squared * squared;
         } while (squared < 1000000);
 
+// Exercise iii For Loops
+        for (int k = 5; k <= 15; k++) {
+            System.out.println(k);
+        }
 
+        for (int t = 0; t <= 100; t = t+2) {
+            System.out.println(t);
+        }
 
+        for (int f = 100; f >= -10; f = f-5) {
+            System.out.println(f);
+        }
+
+        for (long s = 2; s <= 1000000; s = s * s) {
+            System.out.println(s);
+        }
 
     }
 }
