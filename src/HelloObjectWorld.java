ed9709145dcdf675e8f1e66e93478e72bf94c1fc
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-06 15:12:06 -0600

    Added Methods
    Added 3 new methods to get individual info to allow for 3 users to enter data

--- src/HelloObjectWorld.java
+++ src/HelloObjectWorld.java
@@ -6,13 +6,30 @@ import java.util.Scanner;
 public class HelloObjectWorld {
     public static void main(String[] args) {
         Scanner helloScan = new Scanner(System.in);
-        Person newPerson = new Person();
+
+        Person person1 = new Person(getFirstName(helloScan), getLastName(helloScan), getEmail(helloScan));
+        System.out.println("Hello, " + person1.firstName + " " + person1.lastName + ", we have your email as " + person1.email + ".");
+
+        Person person2 = new Person(getFirstName(helloScan), getLastName(helloScan), getEmail(helloScan));
+        System.out.println("Hello, " + person2.firstName + " " + person2.lastName + ", we have your email as " + person2.email + ".");
+
+        Person person3 = new Person(getFirstName(helloScan), getLastName(helloScan), getEmail(helloScan));
+        System.out.println("Hello, " + person3.firstName + " " + person3.lastName + ", we have your email as " + person3.email + ".");
+
+    }
+
+    public static String getFirstName(Scanner helloScan) {
         System.out.println("Please enter your first name: ");
-        newPerson.firstName = helloScan.nextLine();
+        return helloScan.nextLine();
+    }
+
+    public static String getLastName(Scanner helloScan) {
         System.out.println("Please enter your last name: ");
-        newPerson.lastName = helloScan.nextLine();
+        return helloScan.nextLine();
+    }
+
+    public static String getEmail(Scanner helloScan) {
         System.out.println("Please enter your email address: ");
-        newPerson.email = helloScan.nextLine();
-        System.out.println("Hello, " + newPerson.firstName + " " + newPerson.lastName + ", we have your email as " + newPerson.email + ".");
+        return helloScan.nextLine();
     }
 }
