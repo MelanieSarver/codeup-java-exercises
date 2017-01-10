b70e1bcc60adb1affcf944a8cd06516625b9e822
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-10 08:43:38 -0600

    5.6 Exercise 1 complete

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mel on 1/9/17.
+ */
+public class Student {
+    String firstName;
+    String lastName;
+    int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
