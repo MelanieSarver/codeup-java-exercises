d03463f37cdc292354db99e8f086581b9e53dbce
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-10 11:41:05 -0600

    5.6 Exercise 2 complete

---
+++ src/ValidatorTestApp.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by mel on 1/10/17.
+ */
+public class ValidatorTestApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Let's test some exceptions! Woohoo!");
+
+        Validator validator = new Validator(scan);
+
+        int userInteger = validator.getInt("Please enter an integer");
+        System.out.println("You entered " + userInteger + ".");
+        int userRange = validator.getIntWithinRange("Please enter an integer between -100 and 100.", -100, 100);
+        System.out.println("You entered " + userRange + ".");
+        double userDouble = validator.getDouble("Please enter a decimal number.");
+        System.out.println("You entered " + userDouble + ".");
+        double userDoubleRange = validator.getDoubleWithinRange("Please enter a decimal number between 0 and 11", 0.0, 11.0);
+        System.out.println("You entered " + userDoubleRange + ".");
+        String userString = validator.getRequiredString("Please enter your favorite word.");
+        System.out.println("You entered " + userString + ".");
+    }
+}
