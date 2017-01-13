c78d74da9f1712c3a3d68aaee1a345ed563db86a
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-13 15:27:13 -0600

    Readability
    Included some white space to make the code more readable

--- src/ValidatorTestApp.java
+++ src/ValidatorTestApp.java
@@ -6,18 +6,22 @@ import java.util.Scanner;
 public class ValidatorTestApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Let's test some exceptions! Woohoo!");
-
         Validator validator = new Validator(scan);
+
+        System.out.println("Let's test some exceptions! Woohoo!");
 
         int userInteger = validator.getInt("Please enter an integer");
         System.out.println("You entered " + userInteger + ".");
+
         int userRange = validator.getIntWithinRange("Please enter an integer between -100 and 100.", -100, 100);
         System.out.println("You entered " + userRange + ".");
+
         double userDouble = validator.getDouble("Please enter a decimal number.");
         System.out.println("You entered " + userDouble + ".");
+
         double userDoubleRange = validator.getDoubleWithinRange("Please enter a decimal number between 0 and 11", 0.0, 11.0);
         System.out.println("You entered " + userDoubleRange + ".");
+
         String userString = validator.getRequiredString("Please enter your favorite word.");
         System.out.println("You entered " + userString + ".");
     }
