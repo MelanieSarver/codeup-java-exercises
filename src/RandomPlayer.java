81eb16bb5a8b773df17626f6d2b4ae14f14f5818
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-19 17:02:33 -0600

    Exercise 5.13
    1. complete

---
+++ src/RandomPlayer.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * @author Created by mel on 1/18/17.
+ */
+public class RandomPlayer extends Player{
+    @Override
+
+//    Daisy
+    Roshambo generateRoshambo() {
+        Scanner scan = new Scanner(System.in);
+        Validator validator = new Validator(scan);
+        int rps = validator.random(3,1);
+        if (rps == 1) {
+            return Roshambo.ROCK;
+        } else if (rps == 2) {
+            return Roshambo.SCISSORS;
+        } else {
+            return Roshambo.PAPER;
+        }
+    }
+}
+
