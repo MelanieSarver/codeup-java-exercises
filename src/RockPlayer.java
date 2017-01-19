5b8b8c6faca78f43459cfe0f43b6f3171bffa71b
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-19 17:02:49 -0600

    Exercise 5.13
    1. complete

---
+++ src/RockPlayer.java
@@ -0,0 +1,11 @@
+/**
+ * @author Created by mel on 1/18/17.
+ */
+public class RockPlayer extends Player {
+    @Override
+//    Luigi
+    Roshambo generateRoshambo() {
+        return Roshambo.ROCK;
+    }
+}
+
