29c17f1f82d46b146f6b1485f7cc80ac3cbad45c
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-20 16:40:58 -0600

    Exercise 5.14
    1. complete
    method getCount() isn't used, but it was part of the exercise...

---
+++ src/SnapSnapBaa/Countable.java
@@ -0,0 +1,12 @@
+package SnapSnapBaa;
+
+/**
+ * @author Created by mel on 1/20/17.
+ */
+interface Countable {
+    void incrementCount();
+    void resetCount();
+    int getCount();
+    String getCountString();
+
+}
